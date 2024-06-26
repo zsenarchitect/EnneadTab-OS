bf00689567ea68bf3547efe1a43b403153c70439
zsenarchitect <61437205+zsenarchitect@users.noreply.github.com>
2024-06-26 13:40:08 -0400

    update acc

--- Apps/lib/dump scripts/migrate_LHH_to_acc.py
+++ Apps/lib/dump scripts/migrate_LHH_to_acc.py
@@ -5,20 +5,24 @@ from tqdm import tqdm
 J_DRIVE_ACC_FOLDER = "J:\\1643"
 LHH_ACC_FOLDER = "C:\\Users\\szhang\\DC\\ACCDocs\\Ennead Architects LLP\\1643_LHH\\Project Files\\00_1643 LHH"
 
+IGNORE_TYPE_TYPE = [".3dmbak"]
 def main():
     # Collect all jpg files and their paths
-    jpg_files = []
+    work_files = []
     for root, dirs, files in os.walk(J_DRIVE_ACC_FOLDER):
         for file in files:
-            if file.lower().endswith('.jpg') or True:
-                source_file = os.path.join(root, file)
-                relative_path = os.path.relpath(root, J_DRIVE_ACC_FOLDER)
-                destination_dir = os.path.join(LHH_ACC_FOLDER, relative_path)
-                destination_file = os.path.join(destination_dir, file)
-                jpg_files.append((source_file, destination_file))
+            file_extension = os.path.splitext(file)[1].lower()
+            if file_extension in IGNORE_TYPE_TYPE:
+                continue
+            
+            source_file = os.path.join(root, file)
+            relative_path = os.path.relpath(root, J_DRIVE_ACC_FOLDER)
+            destination_dir = os.path.join(LHH_ACC_FOLDER, relative_path)
+            destination_file = os.path.join(destination_dir, file)
+            work_files.append((source_file, destination_file))
 
     # Copy files with progress bar
-    for source_file, destination_file in tqdm(jpg_files, desc="Copying files"):
+    for source_file, destination_file in tqdm(work_files, desc="Copying files"):
         # Create directories in the destination path if they don't exist
         destination_dir = os.path.dirname(destination_file)
         if not os.path.exists(destination_dir):

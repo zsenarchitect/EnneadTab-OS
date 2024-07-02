494ebe796ad2e2378579497a3d9ba7019ec3ef54
zsenarchitect <61437205+zsenarchitect@users.noreply.github.com>
2024-07-02 17:10:44 -0400

    update version

--- Apps/lib/EnneadTab/VERSION_CONTROL.py
+++ Apps/lib/EnneadTab/VERSION_CONTROL.py
@@ -3,4 +3,12 @@ import EXE
 
 def update_EA_dist():
     EXE.try_open_app("EnneadTab_OS_Installer")
-       
+
+
+def unit_test():
+    update_EA_dist()
+
+
+
+if __name__ == "__main__":
+    unit_test()

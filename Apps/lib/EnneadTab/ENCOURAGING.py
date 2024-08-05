50b408b9e0d8c6ad551654fd3e1207470f0a9563
Colin Matthews <25379751+colinlsmatthews@users.noreply.github.com>
2024-08-05 11:23:17 -0400

    bugfix: add error handling to warming quotes file reader

--- Apps/lib/EnneadTab/ENCOURAGING.py
+++ Apps/lib/EnneadTab/ENCOURAGING.py
@@ -13,7 +13,7 @@ def is_hate_encouraging():
     return not CONFIG.get_setting("radio_bt_popup_full", False)
 
 def get_all_warming_quotes():
-    with io.open('{}\\text\\_warming_quotes.txt'.format(ENVIRONMENT.DOCUMENT_FOLDER), "r", encoding = "utf8") as f:
+    with io.open('{}\\text\\_warming_quotes.txt'.format(ENVIRONMENT.DOCUMENT_FOLDER), "r", encoding = "utf8", errors="ignore") as f:
         lines = f.readlines()
     return [x.replace("\n", "") for x  in lines if x != "\n"]
 

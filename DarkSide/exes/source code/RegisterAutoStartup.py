71e4cd48fb5b62f7a303ba507275dd2df81324d8
Sen Zhang <zsenarchitect@gmail.com>
2024-08-07 17:14:12 -0400

    improve register run

--- DarkSide/exes/source code/RegisterAutoStartup.py
+++ DarkSide/exes/source code/RegisterAutoStartup.py
@@ -1,29 +1,51 @@
-"""createa shortcut link at startup folder so it auto run at the start of login.
-This will be helpful for AVD users."""
 import os
 import winshell
+import subprocess
+import _Exe_Util
 
-app = os.path.join(os.environ['USERPROFILE'], 
-                   'Documents', 
-                   'EnneadTab Ecosystem', 
-                   'EA_Dist', 
-                   'Installation', 
-                   'EnneadTab_OS_Installer.exe')
-
-
-
-if os.path.exists(app):
-
+def create_shortcut(app_path, shortcut_name, description):
     startup_folder = os.path.join(os.environ['APPDATA'], 
                                   'Microsoft', 
                                   'Windows', 
                                   'Start Menu', 
                                   'Programs', 
                                   'Startup')
-    shortcut_path = os.path.join(startup_folder, 'EnneadTab_OS_Installer.lnk')
+    shortcut_path = os.path.join(startup_folder, "{}.lnk".format(shortcut_name))
 
     winshell.CreateShortcut(
         Path=shortcut_path,
-        Target=app,
-        Description='EnneadTab OS Installer'
+        Target=app_path,
+        Description=description
     )
+
+    print('Shortcut created successfully in the startup folder.')
+
+def schedule_task(task_name, app_path, interval_minutes):
+    cmd = (
+        'schtasks /create /f /tn "{}" /tr "{}" /sc minute /mo {} /rl highest'.format(task_name, app_path, interval_minutes)
+    )
+
+    try:
+        subprocess.run(cmd, check=True, shell=True)
+        print('Task scheduled successfully to run every {} minutes.'.format(interval_minutes))
+    except subprocess.CalledProcessError as e:
+        print('Failed to schedule the task. Error:', e)
+
+
+@_Exe_Util.try_catch_error
+def main():
+    app = os.path.join(os.environ['USERPROFILE'], 
+                       'Documents', 
+                       'EnneadTab Ecosystem', 
+                       'EA_Dist', 
+                       'Installation', 
+                       'EnneadTab_OS_Installer.exe')
+
+    if os.path.exists(app):
+        create_shortcut(app, 'EnneadTab_OS_Installer', 'EnneadTab OS Installer')
+        schedule_task('EnneadTab_OS_Installer_Task', app, 30)
+    else:
+        print('Application not found at:', app)
+
+if __name__ == "__main__":
+    main()

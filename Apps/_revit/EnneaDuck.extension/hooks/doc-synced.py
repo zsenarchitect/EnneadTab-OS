fbf2ae38321a098801d39ebf0872de80f42f17f5
Sen Zhang <zsenarchitect@gmail.com>
2024-08-08 17:32:16 -0400

    remove publish folder path

--- Apps/_revit/EnneaDuck.extension/hooks/doc-synced.py
+++ Apps/_revit/EnneaDuck.extension/hooks/doc-synced.py
@@ -183,7 +183,7 @@ def update_working_view_name():
     script = "Ennead.tab\\Manage.panel\\working_view_cleanup.pushbutton\\manage_working_view_script.py"
     func_name = "modify_creator_in_view_name"
 
-    fullpath = "{}\\ENNEAD.extension\\{}".format(ENVIRONMENT.PUBLISH_FOLDER_FOR_REVIT, script)
+    fullpath = "{}\\{}".format(ENVIRONMENT.REVIT_PRIMARY_EXTENSION, script)
     import imp
     ref_module = imp.load_source("manage_working_view_script", fullpath)
 
